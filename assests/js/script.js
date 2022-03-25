a4409ec7e2887247960a6e73c2cfc7a685f4950f
Tiago dos saantos da silva <xlzthiago@gmail.com>
2022-03-25 07:59:28 -0300

    adicionando o aquivo de javascript

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,9 @@
-let foot = [
-    {comida: 'arroz', id:1},
-    {comida: 'feijõa', id:2},
-    {comida: 'macarrão', id:3},
-    {comida: 'salada', id:4},
-    {comida: 'suco', id:5},
-    {comida: 'carne', id:6},
-    {comida: 'frango', id:7},
-    {comida: 'café', id:8},
+/*
 
-];
+
 const content = document.querySelector('.content');
 const comida = document.querySelector('.comida');
+const aside = document.querySelector('.aside');
 foot?.map((item, index)=>{
     content.innerHTML +=`
     <div data-name="${index}" draggable="true" class="foots">${item.comida}</div>    
@@ -27,6 +20,21 @@ foots.forEach((item)=>{
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd)
 });
+
+function newArr(){
+
+    aside.innerHTML = '';
+    arr.map((item)=>{
+
+        aside.innerHTML +=`
+            <div class="aside_foots">${item.comida}</div>
+        `;
+        
+
+    });
+
+}
+
 
 
 function dragStart(e){
@@ -54,6 +62,7 @@ let events ={
         let key = +e.currentTarget.lastChild.getAttribute('data-name')
         arr.push(foot[key]);
         console.log(arr);
+        newArr();
     }
 }
 
@@ -61,3 +70,114 @@ Object.keys(events).forEach((item)=>{
     comida.addEventListener(item, events[item]);
 });
 
+*/
+
+let foot = [{
+        comida: 'arroz',
+        id: 1
+    },
+    {
+        comida: 'feijõa',
+        id: 2
+    },
+    {
+        comida: 'macarrão',
+        id: 3
+    },
+    {
+        comida: 'tomate',
+        id: 4
+    },
+    {
+        comida: 'suco',
+        id: 5
+    },
+    {
+        comida: 'carne',
+        id: 6
+    },
+    {
+        comida: 'frango',
+        id: 7
+    },
+    {
+        comida: 'aldace',
+        id: 8
+    },
+    {
+        comida: 'carne magra',
+        id: 9
+    },
+    {
+        comida: 'cheddar',
+        id: 10
+    },
+    {
+        comida: 'queijão parmesão',
+        id: 11
+    },
+    {
+        comida: 'molho barbucue',
+        id: 12
+    },
+    {
+        comida: 'molho barbucue',
+        id: 13
+    },
+    {
+        comida: 'bacon',
+        id: 14
+    },
+    {
+        comida: 'carne de peixe',
+        id: 15
+    },
+];
+
+const categories = document.querySelector('.categoteries_foots');
+const arrowListLeft = document.querySelector('.arrow_list-left');
+const arrowListRight = document.querySelector('.arrow_list-right');
+const arrow = document.querySelector('#arrow');
+const genreFoots = document.querySelector('.genero_foots');
+
+let mouseUp = false;
+let x = categories.style.width = `${foot.length * 120}px`;
+
+let margin = 0;
+let moment = 0
+
+foot?.map((item) => {
+    categories.innerHTML += `<span draggable="true">${item.comida}</span>`;
+})
+
+
+arrowListRight.addEventListener('mouseup', function () {
+    console.log('passou')
+});
+arrowListRight.addEventListener('mousedown', function () {
+    console.log('soltu');
+});
+
+
+
+arrowListLeft.addEventListener('click', function () {
+    let arrow = +x.replace('px', '');
+    console.log(arrow);
+    margin += 150;
+    if (margin > (arrow - 1000)) {
+        margin = (arrow - 1000);
+        return;
+    }
+    categories.style.marginLeft = `-${margin}px`;
+    genreFoots.style.marginLeft = `-100vw`;
+});
+
+arrowListRight.addEventListener('click', function () {
+    margin = margin - 150;
+    if (margin < 0) {
+        margin = 0;
+
+    }
+    categories.style.marginLeft = `-${margin}px`;
+
+});
